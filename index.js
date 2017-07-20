399fbb76ea4409104d310a03795e7ea7b4c6d567
alichen <ali322@gmail.com>
2017-07-20 14:21:56 +0800

    compatible with RN v0.46

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const React = require('react');
+const PropTypes = require('prop-types');
 const {
-  PropTypes,
   Component,
 } = React;
 const { ViewPropTypes } = ReactNative = require('react-native');
